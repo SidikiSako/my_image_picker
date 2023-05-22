c73b546d342941a6abc8dcaf34719214cea4535f
Sidiki Sako <sidiksako@gmail.com>
2023-05-22 23:02:11 +0200

    update ios FLTImagePickerPlugin.m

--- packages/image_picker/image_picker_ios/ios/Classes/FLTImagePickerPlugin.m
+++ packages/image_picker/image_picker_ios/ios/Classes/FLTImagePickerPlugin.m
@@ -224,9 +224,12 @@ typedef NS_ENUM(NSInteger, ImagePickerClassType) { UIImagePickerClassType, PHPic
   UIImagePickerController *imagePickerController = [self createImagePickerController];
   imagePickerController.modalPresentationStyle = UIModalPresentationCurrentContext;
   imagePickerController.delegate = self;
-  imagePickerController.mediaTypes = @[
-    (NSString *)kUTTypeMovie, (NSString *)kUTTypeAVIMovie, (NSString *)kUTTypeVideo,
-    (NSString *)kUTTypeMPEG4
+  // imagePickerController.mediaTypes = @[
+  //   (NSString *)kUTTypeMovie, (NSString *)kUTTypeAVIMovie, (NSString *)kUTTypeVideo,
+  //   (NSString *)kUTTypeMPEG4
+  // ];
+   imagePickerController.mediaTypes = @[
+    (NSString *)kUTTypeMovie
   ];
   imagePickerController.videoQuality = UIImagePickerControllerQualityTypeHigh;
 
